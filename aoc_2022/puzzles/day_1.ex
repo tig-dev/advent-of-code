900dbae603ff9a42ab896f87a063062323349c1d
Cam Skubik-Peplaski <monsterboy2@icloud.com>
2022-12-02 14:35:43 -0500

    Clean up day 1 a little

--- aoc_2022/puzzles/day_1.ex
+++ aoc_2022/puzzles/day_1.ex
@@ -1,26 +1,30 @@
 defmodule Calories do
   def split_input(input), do: String.split(input, "\n")
-  def chunk_input(input), do:
-    split_input(input)
-    |> Enum.chunk_by(&(String.length(&1) == 0))
 
-  def split_elf_invs(calories_list), do:
-    chunk_input(calories_list)
-    |> Enum.filter(&(String.length(Enum.fetch!(&1, 0)) != 0))
-    |> Enum.map(&(Enum.map(&1, fn x -> String.to_integer(x) end)))
+  def chunk_input(input),
+    do:
+      split_input(input)
+      |> Enum.chunk_by(&(String.length(&1) == 0))
 
-  def sum_elf_inv(inv), do:
-    Enum.reduce(inv, fn(x, acc) -> x + acc end)
+  def split_elf_invs(calories_list),
+    do:
+      chunk_input(calories_list)
+      |> Enum.filter(&(String.length(Enum.fetch!(&1, 0)) != 0))
+      |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
 
-  def get_totals(calories_list), do:
-    split_elf_invs(calories_list)
-    |> Enum.map(&(sum_elf_inv(&1)))
+  def sum_elf_inv(inv), do: Enum.reduce(inv, fn x, acc -> x + acc end)
 
-  def get_highest(calories_list, places \\ 1), do:
-    get_totals(calories_list)
-    |> Enum.sort(:desc)
-    |> Enum.take(places)
-    |> Enum.sum()
+  def get_totals(calories_list),
+    do:
+      split_elf_invs(calories_list)
+      |> Enum.map(&sum_elf_inv(&1))
+
+  def get_highest(calories_list, places \\ 1),
+    do:
+      get_totals(calories_list)
+      |> Enum.sort(:desc)
+      |> Enum.take(places)
+      |> Enum.sum()
 end
 
 inputs = File.read!(File.cwd!() <> "/puzzles/inputs/day_1.txt")
